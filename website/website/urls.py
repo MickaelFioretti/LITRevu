a8fbffd82b6b00f6ad2ad07ff1716b62ee7e866e
Mickael <fiorettimickael1@gmail.com>
2023-09-21 14:57:52 +0000

    feat: add page to login

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from authentication.views import index
+from django.contrib.auth.views import LoginView
+from authentication import views
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(
+        "",
+        LoginView.as_view(template_name="login.html", redirect_authenticated_user=True),
+        name="login",
+    ),
     path('admin/', admin.site.urls),
     path('', index),
 ]
