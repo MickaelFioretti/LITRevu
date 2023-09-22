27eb4dbe7d601c6dec0fb51870983ffcb21fc1cb
Mickael <fiorettimickael1@gmail.com>
2023-09-22 11:56:54 +0000

    fix: fix css

--- website/authentication/forms.py
+++ website/authentication/forms.py
@@ -4,29 +4,36 @@ from django import forms
 
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(
+    max_length=150,
+    widget=forms.TextInput(
+        attrs={
+            "class": "w-full px-4 mb-2 border border-slate-600 rounded-lg font-medium",
+            "placeholder": "Nom d'utilisateur",
+        }
+    ),
+    )
+    password1 = forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "w-full px-4 mb-2 border border-slate-600 rounded-lg font-medium",
+                "placeholder": "Mot de passe",
+            }
+        ),
+    )
+    password2 = forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "w-full px-4 mb-2 border border-slate-600 rounded-lg font-medium",
+                "placeholder": "Mot de passe",
+            }
+        ),
+    )
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ["username", "password1", "password2"]
-        widgets = {
-            "username": forms.TextInput(
-                attrs={
-                    "class": "w-full px-4 mb-2 border border-slate-600 rounded-lg font-medium",
-                    "placeholder": "Nom d'utilisateur",
-                }
-            ),
-            "password1": forms.PasswordInput(
-                attrs={
-                    "class": "w-full px-4 mb-2 border border-slate-600 rounded-lg font-medium",
-                    "placeholder": "Mot de passe",
-                }
-            ),
-            "password2": forms.PasswordInput(
-                attrs={
-                    "class": "w-full px-4 mb-2 border border-slate-600 rounded-lg font-medium",
-                    "placeholder": "Confirmation du mot de passe",
-                }
-            ),
-        }
 
 
 class LoginForm(forms.Form):
