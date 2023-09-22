5245f4dedd6b1b4469a072cd4fc655be69c911d2
Mickael <fiorettimickael1@gmail.com>
2023-09-22 10:41:21 +0000

    feat: setup logout

--- website/website/urls.py
+++ website/website/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 import authentication.views
 import flux.views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,6 +12,11 @@ urlpatterns = [
         name="login",
     ),
     path("signup", authentication.views.signup_page, name="signup"),
+    path(
+        "logout",
+        LogoutView.as_view(next_page="login"),
+        name="logout",
+    ),
     path(
         "acceuil/",
         flux.views.index,
