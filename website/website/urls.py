93f4cf9a6bfad063bc1b7418a5110c70895f88f5
Mickael <fiorettimickael1@gmail.com>
2023-09-22 10:39:37 +0000

    feat: setup home page after auth

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from django.urls import path
-from authentication import views
+import authentication.views
+import flux.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
         "",
-        views.LoginPageView.as_view(),
+        authentication.views.LoginPageView.as_view(),
         name="login",
     ),
-    path("signup", views.signup_page, name="signup"),
+    path("signup", authentication.views.signup_page, name="signup"),
+    path(
+        "acceuil/",
+        flux.views.index,
+        name="acceuil"
+    )
 ]
