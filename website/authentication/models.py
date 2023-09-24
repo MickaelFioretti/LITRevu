a440b424dcb24817d4f3185c1940df2e20c1d89a
Mickael Fioretti <fiorettimickael1@gmail.com>
2023-09-24 16:06:55 +0000

    feat: update model to add email for create superuser

--- website/authentication/models.py
+++ website/authentication/models.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
     first_name = None
     last_name = None
-    email = None
+    email = models.EmailField(unique=True, null=True)
 
     USERNAME_FIELD = 'username'
