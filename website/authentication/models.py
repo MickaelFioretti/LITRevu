aaae0a1ea63829b577041c50b690afc755bbc5e1
Mickael Fioretti <fiorettimickael1@gmail.com>
2023-09-29 08:17:55 +0000

    fix: add blank true for email

--- website/authentication/models.py
+++ website/authentication/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     first_name = None
     last_name = None
-    email = models.EmailField(unique=True, null=True)
+    email = models.EmailField(null=True, blank=True)
 
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = "username"
